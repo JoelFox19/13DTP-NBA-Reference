23bbb6c6a0fac025153dc73f5d72643931831ddc
JoelFox19 <jjfox200529@gmail.com>
2022-05-03 12:32:22 +1200

    creating a database

--- application/routes.py
+++ application/routes.py
@@ -1,10 +1,24 @@
 from application import app
 from flask import render_template
+import sqlite3
 
+
+def do_query(query, data=None, fetchone=False):
+   conn = sqlite3.connect('nba.db')
+   cur = conn.cursor()
+   if data is None:
+      cur.execute(query)
+   else:
+      cur.execute(query, data)
+   results = cur.fetchone()if fetchone else cur.fetchall()
+   conn.close()
+   return results
 
 @app.route('/')
 def home():
-   return render_template('home.html',title="Home Page")
+   teams = do_query ("SELECT teamname, image FROM teams")
+   return render_template('home.html',title="Home Page", teams=teams)
+
 
 
 #pip3 install flask_sqlalchemy
