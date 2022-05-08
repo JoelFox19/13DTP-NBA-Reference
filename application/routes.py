bee834278d59661e79e1026f11cfd3c0a8a0f8c5
JoelFox19 <jjfox200529@gmail.com>
2022-05-09 10:51:43 +1200

    working on team page

--- application/routes.py
+++ application/routes.py
@@ -1,3 +1,4 @@
+from re import T
 from application import app
 from flask import render_template
 import sqlite3
@@ -19,6 +20,8 @@ def home():
    teams = do_query ("SELECT teamname, image FROM teams")
    return render_template('home.html',title="Home Page", teams=teams)
 
-
-
+@app.route('/gsw_team/<int:id>')
+def gsw_team():
+   players = do_query ("SELECT * from Players WHERE id=?;",(id,),fetchone=True)
+   return render_template('gsw.html',title="GSW Page", players=players)
 #pip3 install flask_sqlalchemy
