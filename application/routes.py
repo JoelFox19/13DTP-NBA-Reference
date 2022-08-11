718d07d173cd44ad13777e9bcbb03472ce17c721
Your Name <jjfox200529@gmail.com>
2022-08-12 09:26:02 +1200

    completing gallery and 404 pages

--- application/routes.py
+++ application/routes.py
@@ -1,5 +1,5 @@
 from application import app
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, abort
 import sqlite3
 
 
@@ -30,6 +30,8 @@ def pages(teamname):
    teamimage_id = do_query ("SELECT image from display WHERE teamname=?;",(teamname,),fetchone=False)
    players = do_query ("SELECT information, information2, information3, information4 FROM display WHERE teamname=?;",(teamname,),fetchone=False)
    colours = do_query ("SELECT colour FROM display WHERE teamname=?;",(teamname,),fetchone=False)
+   if team_id is None:
+      abort(404)
    return render_template('page.html',title="Team Page", players=players,team_id=team_id,colours=colours,teamimage_id=teamimage_id)
 
 @app.route('/gallery')
@@ -56,6 +58,11 @@ def subscribe():
 @app.route('/about')
 def about():
    return render_template('about.html',title="About Page")
+
+#This query is for my 404 error page so that users cannot go to web pages that do not exist on my website and the 404 page will appear if a user tries to.
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 #pip3 install flask_sqlalchemy
 
